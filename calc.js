26beb97be109e05a04d91c7c35b43635a786286b
lusijanka <lucjamiodek@gmail.com>
2017-07-31 13:35:41 +0200

    initial file added

---
+++ calc.js
@@ -0,0 +1,5 @@
+var a = prompt('What\'s the base parameter?');
+var h = prompt('What\'s the height parameter?');
+var triangleArea = (a*h)/2;
+console.log('Triangle area with the base a =' + a + ' and height = ' + h + 'equals ' + triangleArea);
+alert('Triangle area with the base a =' + a + ' and height = ' + h + 'equals ' + triangleArea);
